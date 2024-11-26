352b1d3e6938276e5daee388f6a54f43d0eac124
joseph <balekialaj@gmail.com>
2024-11-26 15:30:43 +0100

    lancement ordi perso

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class AccueilController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function home()
+    {
+        return $this->render('pages/home/index.html.twig');
+
+    }
+
+    #[Route('/nosChiffres', name: 'nosChiffres')]
+    public function chiffres()
+    {
+        return $this->render('pages/home/nosChiffres.html.twig');
+
+    }
+
+    #[Route('/nosServices', name: 'nosServices')]
+    public function nosServices()
+    {
+        return $this->render('pages/home/nosServices.html.twig');
+
+    }
+
+    #[Route('/nosOffres', name: 'nosOffres')]
+    public function nosOffres()
+    {
+        return $this->render('pages/home/nosOffres.html.twig');
+
+    }
+}
