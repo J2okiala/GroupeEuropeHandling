018ee021a5f414dc031dae4be9764fb84431e103
joseph <balekialaj@gmail.com>
2024-12-11 12:19:03 +0100

    Page-maFiche

--- src/Controller/ProfilCandidatController.php
+++ src/Controller/ProfilCandidatController.php
@@ -22,13 +22,92 @@ class ProfilCandidatController extends AbstractController
         // Faire ce que vous vous voulez avec, comme récuperer des donnés ect..
 
         // Retourner la vue associé
-        return $this->render('pages/utilisateur/profil-candidat.html.twig', ['isSecondaryNavbar' => true]);
+        return $this->render('pages/utilisateur/candidat/profil-candidat.html.twig', ['isSecondaryNavbar' => true]);
     }
 
     #[Route("/deconnexion", name:"deconnexion")]
     public function logout() {
         // peut etre vide
     }
+
+    #[Route('/maFiche', name: 'maFiche')]
+    public function maFiche()
+    {
+        // Récuperer l'utilisateur depuis la session
+        $uilisateur = $this->getUser();
+
+        return $this->render('pages/utilisateur/candidat/ma-fiche.html.twig', [
+            'isSecondaryNavbar' => true,
+        ]);
+    }
+
+
+    #[Route('/modifierMesInformations', name: 'modifierMesInformations')]
+    public function modifierMesInformations()
+    {
+        // Récuperer l'utilisateur depuis la session
+        $uilisateur = $this->getUser();
+
+        return $this->render('pages/utilisateur/candidat/modifier-mes-informations.html.twig', [
+            'isSecondaryNavbar' => true,
+        ]);
+    }
+
+    #[Route('/mesCandidatures', name: 'mesCandidatures')]
+    public function mesCandidatures()
+    {
+        // Récuperer l'utilisateur depuis la session
+        $uilisateur = $this->getUser();
+
+        return $this->render('pages/utilisateur/candidat/mes-candidatures.html.twig', [
+            'isSecondaryNavbar' => true,
+        ]);
+    }
+
+    #[Route('/mesAlertes', name: 'mesAlertes')]
+    public function mesAlertes()
+    {
+        // Récuperer l'utilisateur depuis la session
+        $uilisateur = $this->getUser();
+
+        return $this->render('pages/utilisateur/candidat/mes-alertes.html.twig', [
+            'isSecondaryNavbar' => true,
+        ]);
+    }
+
+    #[Route('/modifierMesPiecesJointes', name: 'modifierMesPiecesJointes')]
+    public function modifierMesPiecesJointes()
+    {
+        // Récuperer l'utilisateur depuis la session
+        $uilisateur = $this->getUser();
+
+        return $this->render('pages/utilisateur/candidat/modifier-mes-pieces-jointes.html.twig', [
+            'isSecondaryNavbar' => true,
+        ]);
+    }
+
+    #[Route('/mesIdentifiantsDeConnexion', name: 'mesIdentifiantsDeConnexion')]
+    public function mesIdentifiantsDeConnexion()
+    {
+        // Récuperer l'utilisateur depuis la session
+        $uilisateur = $this->getUser();
+
+        return $this->render('pages/utilisateur/candidat/mes-identifiants-de-connexion.html.twig', [
+            'isSecondaryNavbar' => true,
+        ]);
+    }
+
+    #[Route('/desactiverMonCompte', name: 'desactiverMonCompte')]
+    public function desactiverMonCompte()
+    {
+        // Récuperer l'utilisateur depuis la session
+        $uilisateur = $this->getUser();
+
+        return $this->render('pages/utilisateur/candidat/desactiver-mon-compte.html.twig', [
+            'isSecondaryNavbar' => true,
+        ]);
+    }
+
 
 
 }
---
+++ src/Entity/Employeur.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmployeurRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: EmployeurRepository::class)]
+class Employeur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank]
+    private ?string $nomEntreprise = null;
+
+    #[ORM\OneToOne(inversedBy: 'employeur', targetEntity: Utilisateur::class, cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Utilisateur $utilisateur = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomEntreprise(): ?string
+    {
+        return $this->nomEntreprise;
+    }
+
+    public function setNomEntreprise(string $nomEntreprise): self
+    {
+        $this->nomEntreprise = $nomEntreprise;
+
+        return $this;
+    }
+
+    public function getUtilisateur(): ?Utilisateur
+    {
+        return $this->utilisateur;
+    }
+
+    public function setUtilisateur(Utilisateur $utilisateur): self
+    {
+        $this->utilisateur = $utilisateur;
+
+        return $this;
+    }
+}
