34fb33cbe5f9ae448709a443e05a8d48c628e4b1
J2okiala <balekialaj@gmail.com>
2026-03-04 19:08:57 +0100

    Joseph: Forcer Symfony à ne JAMAIS charger Dotenv en prod.

--- public/index.php
+++ public/index.php
@@ -2,8 +2,13 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/vendor/autoload.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+$env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'prod';
+$debug = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? false;
+
+$kernel = new Kernel($env, (bool) $debug);
+$request = Symfony\Component\HttpFoundation\Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
